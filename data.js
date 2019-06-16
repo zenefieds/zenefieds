cae8226b4683d8aa32197bfee64884bf1e537eff
Kevin Pozzi <kpozzi@zenefits.com>
2019-06-16 13:02:43 -0700

    deleted unnecessary files and created some seed data

---
+++ data.js
@@ -0,0 +1,38 @@
+const data = [
+    {
+        title: 'Mens Unique Leather Band Dress Wrist Watch',
+        description: 'Top of the line watch that you should buy for a lot right now',
+        photos: ['url'],
+        condition: 'great',
+        price: 80,
+        city: 'San Francisco',
+        state: 'CA',
+        category: 'Clothing & Accessories',
+        status: 'active',
+        owner: 'Shujian'
+    },
+    {
+        title: 'My old left sock',
+        description: 'My old sock that you probably dont want to buy',
+        photos: ['url'],
+        condition: 'poor',
+        price: 2000,
+        city: 'Tempe',
+        state: 'AZ',
+        category: 'Clothing & Accessories',
+        status: 'active',
+        owner: 'Kevin'
+    },
+    {
+        title: 'Stuffed animal',
+        description: 'My baby hated it, maybe yours won\'t',
+        photos: ['url'],
+        condition: 'new',
+        price: 2000,
+        city: 'San Francisco',
+        state: 'CA',
+        category: 'Misc',
+        status: 'active',
+        owner: 'Kevin'
+    },
+]
---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      hi from react
+    </div>
+  );
+}
+
+export default App;
