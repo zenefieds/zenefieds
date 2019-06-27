e794f890a47ca6e1d505df3e8c75a0ada6839694
Shujian Hou <shou@zenefits.com>
2019-06-27 13:42:57 -0700

    switch for  edit and favorite icon based on currentpage

--- src/App/Item_Card/ItemCard.js
+++ src/App/Item_Card/ItemCard.js
@@ -7,7 +7,6 @@ class ItemCard extends Component {
         super(props);
         this.state = {
             isLiked: false,
-
         }
         this.toggleLiked = this.toggleLiked.bind(this);
     }
@@ -28,13 +27,22 @@ class ItemCard extends Component {
                     <div className='priceChip'>
                         ${this.props.item.price}
                     </div>
+                    { this.props.currentPage !== 'MyActivity' ? 
                     <span className="heart-icon" onClick={this.toggleLiked}>
                         { this.state.isLiked ? <i class="material-icons">favorite</i> : <j class="material-icons">favorite</j>}
                     </span>
+                    : <div></div> }
                 </div>
                 <div className='categoryText'>{this.props.item.category}</div>
                 <div className='titleText'>{this.props.item.title}</div>
-                <div className='ownerDateText'>{this.props.item.owner}&nbsp;&nbsp;&#8226;&nbsp;&nbsp;{this.props.item.date}</div>
+                <div className='textAndEdit'>
+                    <div className='ownerDateText'>
+                    {this.props.item.owner}&nbsp;&nbsp;&#8226;&nbsp;&nbsp;{this.props.item.date}
+                    </div>
+                    {this.props.currentPage === 'MyActivity' ? <div className='pencilEdit'>
+                        <i class="material-icons">border_color</i>
+                    </div> : <div></div>}
+                </div>
             </div>
         )
     }
